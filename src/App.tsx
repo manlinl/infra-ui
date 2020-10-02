0d222c9d648afb7c109a72c3ec8dc206ff3a38a2
Manlin Li <manlinli@gmail.com>
2020-10-02 11:46:46 -0700

    Import @material-ui/core

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Box from '@material-ui/core/Box';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box component="span" display="block">Hello World</Box>
     </div>
   );
 }
